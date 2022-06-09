cba35a7da2acdac7fb3a10278f40c37cfae7e27a
alekate <alekate2003@hotmail.com>
2022-06-09 04:24:03 -0300

    Base del juego
    niveles, vicotria, retry, preload , mainmeu.

---
+++ src/scene/retry.js
@@ -0,0 +1,51 @@
+import Button from "../js/button.js";
+
+var score;
+
+export class retry extends Phaser.Scene {
+    
+    
+    constructor() {
+        super("retry");
+    }
+    init(data) { //llama data de otra escena
+        score = data.score;
+    }
+
+
+    create() {
+        this.add
+          .image(this.cameras.main.centerX, this.cameras.main.centerY, "Fondo_Over") //buscar pantalla retry y poner ak cargar con la key.
+          .setScale(1.1);
+    
+        this.add.image(
+          this.cameras.main.centerX,
+          this.cameras.main.centerY / 1.5,
+          "gameover_logo" //hacer logo de game over mismo que fondo
+        )
+        .setScale(1.1);
+    
+        this.add
+          .text(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY,
+            `Puntaje de nivel alcanzado: ${score}`
+          )
+          .setOrigin(0.5)
+          .setScale(1.2);
+    
+        const boton = new Button(
+          this.cameras.main.centerX,
+          this.cameras.main.centerY + this.cameras.main.centerY / 3,
+          "Reintentar",
+          this,
+          () => {
+            this.scene.start("mainmenu"); // te manda al menu principal
+          }
+        );
+      }
+
+
+
+
+}
